d25a078ed5427b03c7e58737352710f0b1bb7ca5
Ron Viscuso <13020754+rviscuso@users.noreply.github.com>
2021-07-07 13:24:57 -0700

    Update index.js

--- actions/index.js
+++ actions/index.js
@@ -3,6 +3,6 @@ const core = require('@actions/core');
 core.info('starting timer');
 setTimeout(() => {
   core.info('timer is over');
-  core.setOutput('res', body);
+  core.setOutput('res', 'xxxxxxxxx');
   core.setFailed(err);
 }, 20000)
